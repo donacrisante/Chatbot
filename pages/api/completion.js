354e8665bf5212afc4f2ab2afd429ddf3405c720
Donatella Crisante <dona.crisante@zoho.com>
2023-07-19 11:52:00 +0200

    create chatbot

---
+++ pages/api/completion.js
@@ -0,0 +1,19 @@
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { Configuration, OpenAIApi } from 'openai-edge'
+ 
+export const runtime = 'edge'
+ 
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(config)
+ 
+export default async function handler(req, res) {
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: [{ role: 'user', content: 'What is love?' }]
+  })
+  const stream = OpenAIStream(response)
+  return new StreamingTextResponse(stream)
+}
